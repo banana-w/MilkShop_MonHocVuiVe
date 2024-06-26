2546de330db8d346dc89c80738db0e05ed140f6e
banana-w <phuonghiepthuan56@gmail.com>
2024-06-27 02:03:40 +0700

    wpfOrder

--- MilkShop.WPF/MainWindow.xaml.cs
+++ MilkShop.WPF/MainWindow.xaml.cs
@@ -35,5 +35,11 @@ namespace MilkShop.WPF
             p.Owner = this;
             p.Show();
         }
+        private void Open_wOrder_Click(object sender, RoutedEventArgs e)
+        {
+            var p = new wOrder();
+            p.Owner = this;
+            p.Show();
+        }
     }
 }
